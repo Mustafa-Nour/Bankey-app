7325cd587e6a975884eb088cdc6a7e174e222b9b
Mustafa-Nour <109715325+Mustafa-Nour@users.noreply.github.com>
2026-01-10 12:57:24 +0200

    setting upg a tabBarController

---
+++ Bankey/Utills/UIViewController+utills.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewController+utills.swift
+//  Bankey App
+//
+//  Created by Mustafa Nour on 10/01/2026.
+//
+
+import Foundation
---
+++ MainViewController .swift
@@ -0,0 +1,8 @@
+//
+//  MainViewController .swift
+//  Bankey App
+//
+//  Created by Mustafa Nour on 10/01/2026.
+//
+
+import Foundation
