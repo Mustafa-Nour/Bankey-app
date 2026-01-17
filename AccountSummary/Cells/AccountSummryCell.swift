8e61d631953260715495d73eac715db636f5e93e
Mustafa-Nour <109715325+Mustafa-Nour@users.noreply.github.com>
2026-01-17 19:04:22 +0200

    feat: add elements and style it.

---
+++ AccountSummary/Cells/AccountSummryCell.swift
@@ -0,0 +1,30 @@
+//
+//  AccountSummryCell.swift
+//  Bankey App
+//
+//  Created by Mustafa Nour on 17/01/2026.
+//
+
+import Foundation
+import UIKit
+
+class AccountSummryCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: restorationIdentifier, reuseIdentifier: <#T##String?#>)
+        setup()
+        layout()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension AccountSummryCell {
+    private func setup() {
+        
+    }
+    
+    private func layout() {
+        
+    }
+}
