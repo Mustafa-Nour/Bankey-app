a5ed8c810fc8e2e26639505c5323721af4e430fa
Mustafa-Nour <109715325+Mustafa-Nour@users.noreply.github.com>
2026-01-20 16:33:54 +0200

    feat: format bank balcnce(NSAttributedString)

--- AccountSummary/Cells/AccountSummryCell.swift
+++ AccountSummary/Cells/AccountSummryCell.swift
@@ -20,7 +20,7 @@ class AccountSummryCell: UITableViewCell {
     let chevronImageView = UIImageView()
     
     static let reuseID = "AccountSummaryCell"
-    static let rowHeight: CGFloat = 100
+    static let rowHeight: CGFloat = 112
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -105,5 +105,20 @@ extension AccountSummryCell {
             trailingAnchor.constraint(equalToSystemSpacingAfter: chevronImageView.trailingAnchor, multiplier: 1)
             ])
         
+    }
+    
+    private func makeFormattedBalance(dollars: String, cents: String) -> NSAttributedString {
+        let dollarSignAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.preferredFont(forTextStyle: .callout), .baselineOffset: 8]
+        let dollarAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.preferredFont(forTextStyle: .title1)]
+        let centAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.preferredFont(forTextStyle: .footnote),.baselineOffset: 8]
+        
+        let rootString = NSMutableAttributedString(string: "$", attributes: dollarSignAttributes)
+        let dollarString = NSAttributedString(string: dollars, attributes: dollarAttributes)
+        let centString = NSAttributedString(string: cents, attributes: centAttributes)
+        
+        rootString.append(dollarString)
+        rootString.append(centString)
+       
+        return rootString
     }
 }
