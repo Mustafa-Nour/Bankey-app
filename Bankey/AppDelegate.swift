532f21d6d986be5243b0897cad7d3f26b755b880
Mustafa-Nour <109715325+Mustafa-Nour@users.noreply.github.com>
2026-02-06 12:10:56 +0200

    Refactoring : general appDelegate Clean up

--- Bankey/AppDelegate.swift
+++ Bankey/AppDelegate.swift
@@ -21,37 +21,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         loginViewController.delegate = self
         onboardingContainer.delegate = self
-        let vc = mainViewController
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.makeKeyAndVisible()
-        window?.backgroundColor = .systemBackground
-        
-        vc.setStatusBar()
-        let appearance = UINavigationBarAppearance()
-        appearance.configureWithOpaqueBackground()
-        appearance.backgroundColor = appColor
-        appearance.shadowColor = .clear
-        
-        UINavigationBar.appearance().standardAppearance = appearance
-        UINavigationBar.appearance().scrollEdgeAppearance = appearance
-        UINavigationBar.appearance().compactAppearance = appearance
-
-        window?.rootViewController = vc
-        
+       
+        displayLogin()
         return true
     }
+    
+    private func displayLogin() {
+        setRootViewController(loginViewController)
+    }
+    
+    private func displayNextScreen() {
+        if LocalState.hasOnboraded {
+            prepMainView()
+            setRootViewController(mainViewController)
+        } else {
+            setRootViewController(onboardingContainer)
+        }
+    }
+    
+    private func prepMainView() {
+        mainViewController.setStatusBar()
+        UINavigationBar.appearance().isTranslucent = false
+        UINavigationBar.appearance().backgroundColor = appColor
+        
+    }
+    
 }
 
 //login delegate
 extension AppDelegate: LoginViewControllerDelegate {
     func didLogin() {
-        loginViewController.SignInButton.configuration?.showsActivityIndicator = false
-        if LocalState.hasOnboraded {
-            setRootViewController(mainViewController)
-            
-        } else  {
-            setRootViewController(onboardingContainer)
-        }
+        displayNextScreen()
     }
 }
 
